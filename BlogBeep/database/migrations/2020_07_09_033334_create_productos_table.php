fc25fd93ffaff9ef0a28c7c512283f9273051f1d
Franco Viera <vierafranco1@gmail.com>
2020-07-09 00:39:15 -0300

    se creo el proyecto con los recursos necesesarios

---
+++ BlogBeep/database/migrations/2020_07_09_033334_create_productos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->String('tipo');
+            $table->timestamps();
+        });
+
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->String('nombre');
+            $table->integer('precio');
+            $table->integer('cantidad');
+            $table->bigInteger('id_categoria')->unsigned();
+            $table->foreign('id_categoria')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+        Schema::dropIfExists('categorias');
+    }
+}
---
+++ BlogBeep/database/migrations/2020_07_09_033351_create_compras_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComprasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->String('nombre');
+            $table->integer('precio');
+            $table->integer('cantidad');
+            $table->boolean('pago');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compras');
+    }
+}
